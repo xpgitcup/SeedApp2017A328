eecae6e414ff57df663d01e1a384fc90de20e1ff
CHUYUN527-LXP\LiXiaoping <xpli126@126.com>
2017-04-03 18:21:35 +0800

    修改cookie的设置

--- grails-app/assets/javascripts/cn/edu/cup/common/mainEasyUI.js
+++ grails-app/assets/javascripts/cn/edu/cup/common/mainEasyUI.js
@@ -9,14 +9,14 @@ $(function () {
     console.info("这是整体布局...");
 
     //读取当前的Panel
-    currentAccordion = readCookie("currentAccordion", "底层管理");
+    currentAccordion = readCookie("currentAccordion", 0);
     console.info("上一次停留在：" + currentAccordion);
     mainSystemMenuDiv = $("#mainSystemMenuDiv");
 
     mainSystemMenuDiv.accordion({
         onSelect: function (title, index) {
             console.info("选择：" + title + "---" + index);
-            $.cookie("currentAccordion", title, {path: '/'});
+            $.cookie("currentAccordion", index, {path: '/'});
         }
     });
     mainSystemMenuDiv.accordion('select', currentAccordion);
